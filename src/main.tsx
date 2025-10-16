b4eb2a51e2b675ea2523f0540efebf3179d612d3
Sebastian Basto <sebas2006rod@gmail.com>
2025-10-16 13:51:49 -0500

    feat: wrap App with AuthProvider and add ToastContainer

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+import { AuthProvider } from './context/AuthContext';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+      {/* 🔹 NUEVO: toasts globales */}
+      <ToastContainer position="top-right" autoClose={3000} />
+    </AuthProvider>
   </React.StrictMode>,
 );

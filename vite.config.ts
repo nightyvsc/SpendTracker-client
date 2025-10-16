a3cd55d0ba47a4c1e999eac94ce386bdb08cdd09
Sebastian Basto <sebas2006rod@gmail.com>
2025-10-16 13:27:33 -0500

    chore: configure @ alias for src imports in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
-// https://vite.dev/config/
+// Configuración de Vite
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // 🔹 Permite usar '@' para importar desde /src
+    },
+  },
+});

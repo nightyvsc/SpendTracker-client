006e287b69c6432f9464e6cb953bb496b0b52717
sebasbasto <160449750+sebasbasto@users.noreply.github.com>
2025-10-16 20:03:56 -0500

    Revert "Feature/dashboard reports"

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import SignIn from './pages/SignIn.tsx';
-import PrivateRoute from './routes/PrivateRoute';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import Dashboard from './pages/Dashboard.tsx';
-import SignUp from './pages/SignUp.tsx';
+function App() {
+  const [count, setCount] = useState(0)
 
-export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* pública */}
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-
-        {/* protegidas */}
-        <Route element={<PrivateRoute />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          {/* Aquí luego agregamos /reports y demás */}
-        </Route>
-
-        {/* redirecciones por defecto */}
-        <Route path="/" element={<Navigate to="/signin" replace />} />
-        <Route path="*" element={<Navigate to="/signin" replace />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
+
+export default App
